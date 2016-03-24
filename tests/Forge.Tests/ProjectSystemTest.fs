426234835c2cd703450978c000ffd7248633f782
Jerold Haas <jeroldhaas@gmail.com>
2016-03-23 22:05:13 -0400

    redundant fixture decl

--- tests/Forge.Tests/ProjectSystemTest.fs
+++ tests/Forge.Tests/ProjectSystemTest.fs
@@ -8,156 +8,154 @@ open Forge.ProjectSystem
 open NUnit.Framework
 open FsUnit
 
-[<TestFixture>]
-module ``ProjectSystem Tests`` =
 
-    [<Test>]
-    let ``parse - AST gets all project files`` () =
-        let projectFile = FsProject.parse astInput
-        System.Diagnostics.Debug.WriteLine projectFile
-        projectFile.SourceFiles.AllFiles() |> Seq.length |> should be (equal 3)
+[<Test>]
+let ``parse - AST gets all project files`` () =
+    let projectFile = FsProject.parse astInput
+    System.Diagnostics.Debug.WriteLine projectFile
+    projectFile.SourceFiles.AllFiles() |> Seq.length |> should be (equal 3)
 
-    [<Test>]
-    let ``parse - AST gets all references`` () =
-        let projectFile = FsProject.parse astInput
-        projectFile.References |> Seq.length|> should be (equal 5)
+[<Test>]
+let ``parse - AST gets all references`` () =
+    let projectFile = FsProject.parse astInput
+    projectFile.References |> Seq.length|> should be (equal 5)
 
-    [<Test>]
-    let ``parse - AST gets correct settings`` () =
-        let projectFile = FsProject.parse astInput
-        let s = projectFile.Settings
-        s.Configuration.Data |> should be (equal ^ Some "Debug")
-        s.Platform.Data |> should be (equal ^ Some "AnyCPU")
-        s.SchemaVersion.Data |> should be (equal ^ Some "2.0")
-        s.ProjectGuid.Data |> should be (equal ^ Some ^ System.Guid.Parse "fbaf8c7b-4eda-493a-a7fe-4db25d15736f")
-        s.OutputType.Data |> should be (equal ^ Some OutputType.Library)
-        s.TargetFrameworkVersion.Data |> should be (equal ^ Some "v4.5")
-        s.AssemblyName.Data |> should be (equal ^ Some "Test")
+[<Test>]
+let ``parse - AST gets correct settings`` () =
+    let projectFile = FsProject.parse astInput
+    let s = projectFile.Settings
+    s.Configuration.Data |> should be (equal ^ Some "Debug")
+    s.Platform.Data |> should be (equal ^ Some "AnyCPU")
+    s.SchemaVersion.Data |> should be (equal ^ Some "2.0")
+    s.ProjectGuid.Data |> should be (equal ^ Some ^ System.Guid.Parse "fbaf8c7b-4eda-493a-a7fe-4db25d15736f")
+    s.OutputType.Data |> should be (equal ^ Some OutputType.Library)
+    s.TargetFrameworkVersion.Data |> should be (equal ^ Some "v4.5")
+    s.AssemblyName.Data |> should be (equal ^ Some "Test")
 
-    [<Test>]
-    let ``parse - add new file``() =
-        let pf = FsProject.parse astInput
-        let f = {SourceFile.Include = "Test.fsi"; Condition = None; OnBuild = BuildAction.Compile; Link = None; Copy = None}
-        let pf' = FsProject.addSourceFile "/" f pf
-        let files = pf'.SourceFiles.AllFiles()
-        TestContext.WriteLine  (sprintf "%A" files)
-        pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 4)
+[<Test>]
+let ``parse - add new file``() =
+    let pf = FsProject.parse astInput
+    let f = {SourceFile.Include = "Test.fsi"; Condition = None; OnBuild = BuildAction.Compile; Link = None; Copy = None}
+    let pf' = FsProject.addSourceFile "/" f pf
+    let files = pf'.SourceFiles.AllFiles()
+    TestContext.WriteLine  (sprintf "%A" files)
+    pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 4)
 
-    [<Test>]
-    let ``parse - add duplicate file``() =
-        let pf = FsProject.parse astInput
-        let f = {SourceFile.Include = "FixProject.fs"; Condition = None; OnBuild = BuildAction.Compile; Link = None; Copy = None}
-        let pf' = FsProject.addSourceFile "/" f pf
-        let files = pf'.SourceFiles.AllFiles()
-        TestContext.WriteLine (sprintf "%A" files)
-        pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 3)
+[<Test>]
+let ``parse - add duplicate file``() =
+    let pf = FsProject.parse astInput
+    let f = {SourceFile.Include = "FixProject.fs"; Condition = None; OnBuild = BuildAction.Compile; Link = None; Copy = None}
+    let pf' = FsProject.addSourceFile "/" f pf
+    let files = pf'.SourceFiles.AllFiles()
+    TestContext.WriteLine (sprintf "%A" files)
+    pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 3)
 
-    [<Test>]
-    let ``parse - remove file``() =
-        let pf = FsProject.parse astInput
-        let f = "FixProject.fs"
-        let pf' = FsProject.removeSourceFile f pf
-        pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 2)
+[<Test>]
+let ``parse - remove file``() =
+    let pf = FsProject.parse astInput
+    let f = "FixProject.fs"
+    let pf' = FsProject.removeSourceFile f pf
+    pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 2)
 
-    [<Test>]
-    let ``parse - remove not existing file``() =
-        let pf = FsProject.parse astInput
-        let f = "FixProject2.fs"
-        let pf' = FsProject.removeSourceFile f pf
-        pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 3)
+[<Test>]
+let ``parse - remove not existing file``() =
+    let pf = FsProject.parse astInput
+    let f = "FixProject2.fs"
+    let pf' = FsProject.removeSourceFile f pf
+    pf'.SourceFiles.AllFiles() |> Seq.length |> should be (equal 3)
 
-    [<Test>]
-    let ``parse  - order file``() =
-        let pf = FsProject.parse astInput
-        let pf' = pf |> FsProject.moveUp "a_file.fs" |> FsProject.moveUp "a_file.fs" 
-        let files = pf'.SourceFiles.AllFiles()
-        files |> Seq.head |> should be (equal "a_file.fs")
-        files |> Seq.length |> should be (equal 3)
+[<Test>]
+let ``parse  - order file``() =
+    let pf = FsProject.parse astInput
+    let pf' = pf |> FsProject.moveUp "a_file.fs" |> FsProject.moveUp "a_file.fs" 
+    let files = pf'.SourceFiles.AllFiles()
+    files |> Seq.head |> should be (equal "a_file.fs")
+    files |> Seq.length |> should be (equal 3)
 
-    [<Test>]
-    let ``parse - add reference``() =
-        let pf = FsProject.parse astInput
-        let r = {Reference.Empty with Include = "System.Xml"}
-        let pf' = FsProject.addReference r pf
-        pf'.References |> Seq.length |> should be (equal 6)
+[<Test>]
+let ``parse - add reference``() =
+    let pf = FsProject.parse astInput
+    let r = {Reference.Empty with Include = "System.Xml"}
+    let pf' = FsProject.addReference r pf
+    pf'.References |> Seq.length |> should be (equal 6)
 
-    [<Test>]
-    let ``parse - add existing reference``() =
-        let pf = FsProject.parse astInput
-        let r = {Reference.Empty with Include = "System"}
-        let pf' = FsProject.addReference r pf
-        pf'.References |> Seq.length |> should be (equal 5)
+[<Test>]
+let ``parse - add existing reference``() =
+    let pf = FsProject.parse astInput
+    let r = {Reference.Empty with Include = "System"}
+    let pf' = FsProject.addReference r pf
+    pf'.References |> Seq.length |> should be (equal 5)
 
-    [<Test>]
-    let ``parse - remove reference``() =
-        let pf = FsProject.parse astInput
-        let r = {Reference.Empty with Include = "System"}
-        let pf' = FsProject.removeReference r pf
-        pf'.References |> Seq.length |> should be (equal 4)
+[<Test>]
+let ``parse - remove reference``() =
+    let pf = FsProject.parse astInput
+    let r = {Reference.Empty with Include = "System"}
+    let pf' = FsProject.removeReference r pf
+    pf'.References |> Seq.length |> should be (equal 4)
 
-    [<Test>]
-    let ``parse - remove not existing reference``() =
-        let pf = FsProject.parse astInput
-        let r = {Reference.Empty with Include = "System.Xml"}
-        let pf' = FsProject.removeReference r pf
-        pf'.References |> Seq.length |> should be (equal 5)
+[<Test>]
+let ``parse - remove not existing reference``() =
+    let pf = FsProject.parse astInput
+    let r = {Reference.Empty with Include = "System.Xml"}
+    let pf' = FsProject.removeReference r pf
+    pf'.References |> Seq.length |> should be (equal 5)
 
-    [<Test>]
-    let ``parse - move up``() =
-        let pf = FsProject.parse astInput
-        let pf' = FsProject.moveUp "App.config" pf
-        let files = pf'.SourceFiles.AllFiles()
-        TestContext.WriteLine (sprintf "%A" files)
-        pf'.SourceFiles.AllFiles()
-        |> Seq.head
-        |> should equal "app.config"
-        
-    [<Test>]
-    let ``parse - move up nonexistent file``() =
-        let pf = FsProject.parse astInput
-        let pf' = FsProject.moveUp "dont_exist.fs" pf
-        pf'.SourceFiles.AllFiles()
-        |> Seq.contains "dont_exist.fs"
-        |> should equal false
+[<Test>]
+let ``parse - move up``() =
+    let pf = FsProject.parse astInput
+    let pf' = FsProject.moveUp "App.config" pf
+    let files = pf'.SourceFiles.AllFiles()
+    TestContext.WriteLine (sprintf "%A" files)
+    pf'.SourceFiles.AllFiles()
+    |> Seq.head
+    |> should equal "app.config"
+    
+[<Test>]
+let ``parse - move up nonexistent file``() =
+    let pf = FsProject.parse astInput
+    let pf' = FsProject.moveUp "dont_exist.fs" pf
+    pf'.SourceFiles.AllFiles()
+    |> Seq.contains "dont_exist.fs"
+    |> should equal false
 
-    [<Test>]
-    let ``parse - move down``() =
-        let pf = FsProject.parse astInput
-        let pf' = FsProject.moveDown "app.config" pf
-        let files = pf'.SourceFiles.AllFiles()
-        TestContext.WriteLine (sprintf "%A" files)
-        pf'.SourceFiles.AllFiles()
-        |> Seq.rev
-        |> Seq.head
-        |> should equal "app.config"
+[<Test>]
+let ``parse - move down``() =
+    let pf = FsProject.parse astInput
+    let pf' = FsProject.moveDown "app.config" pf
+    let files = pf'.SourceFiles.AllFiles()
+    TestContext.WriteLine (sprintf "%A" files)
+    pf'.SourceFiles.AllFiles()
+    |> Seq.rev
+    |> Seq.head
+    |> should equal "app.config"
 
-    [<Test>]
-    let ``parse - move down nonexistent files ``() =
-        let pf = FsProject.parse astInput
-        let pf' = FsProject.moveDown "dont_exist.fs" pf
-        pf'.SourceFiles.AllFiles()
-        |> Seq.contains "dont_exist.fs"
-        |> should equal false
+[<Test>]
+let ``parse - move down nonexistent files ``() =
+    let pf = FsProject.parse astInput
+    let pf' = FsProject.moveDown "dont_exist.fs" pf
+    pf'.SourceFiles.AllFiles()
+    |> Seq.contains "dont_exist.fs"
+    |> should equal false
 
-    // TODO: complete test code
-    [<Test>]
-    let ``parse - add above (?)``() =
-        true |> should equal true
+// TODO: complete test code
+[<Test>]
+let ``parse - add above (?)``() =
+    true |> should equal true
 
-    // TODO: complete test code
-    [<Test>]
-    let ``parse - add below (?)``() =
-        true |> should equal true
+// TODO: complete test code
+[<Test>]
+let ``parse - add below (?)``() =
+    true |> should equal true
 
-    [<Test>]
-    let ``parse - remove dir``() =
-        let pf = FsProject.parse projectWithDirs
-        let pf' = FsProject.removeDirectory "OneDirectory" pf
-        let files = pf'.SourceFiles.AllFiles()
-        TestContext.WriteLine (sprintf "%A" files)
-        pf'.SourceFiles.AllFiles()
-        |> Seq.length
-        |> should be (lessThan 4)
+[<Test>]
+let ``parse - remove dir``() =
+    let pf = FsProject.parse projectWithDirs
+    let pf' = FsProject.removeDirectory "OneDirectory" pf
+    let files = pf'.SourceFiles.AllFiles()
+    TestContext.WriteLine (sprintf "%A" files)
+    pf'.SourceFiles.AllFiles()
+    |> Seq.length
+    |> should be (lessThan 4)
 
 //    [<Test>]
 //    let ``parse - rename file``() =
@@ -179,16 +177,16 @@ module ``ProjectSystem Tests`` =
 //        |> Seq.contains "renameddirectory"
 //        |> should equal true
 
-    [<Test>]
-    let ``parse - list references``() =
-        let pf = FsProject.parse astInput
-        let refs = FsProject.listReferences pf
-        refs.Length |> should be (equal 5)
+[<Test>]
+let ``parse - list references``() =
+    let pf = FsProject.parse astInput
+    let refs = FsProject.listReferences pf
+    refs.Length |> should be (equal 5)
 
-    [<Test>]
-    let ``parse - list files``() =
-        let pf = FsProject.parse astInput
-        let files = FsProject.listSourceFiles pf
-        files.Length |> should be (equal 3)
+[<Test>]
+let ``parse - list files``() =
+    let pf = FsProject.parse astInput
+    let files = FsProject.listSourceFiles pf
+    files.Length |> should be (equal 3)
 
 // PathHelper is internal - no direct testing available
